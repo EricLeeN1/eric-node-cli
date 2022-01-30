9c0629412ec0995dfa57617fa77255d5b568e0e4
EricLeeN1 <179310009@qq.com>
2022-01-30 08:41:00 +0800

    feac:添加常用工具库

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 
-// #! 符号的名称叫 Shebang，用于指定脚本的解释程序
+// #! 符号的名称叫 Shellbang，用于指定脚本的解释程序
 // Node CLI 应用入口文件必须要有这样的文件头
 // 如果是Linux 或者 macOS 系统下还需要修改此文件的读写权限为 755
 // 具体就是通过 chmod 755 cli.js 实现修改
@@ -8,6 +8,7 @@
 // 用于检查入口文件是否正常执行
 console.log("eric-node-cli working~");
 
+const program = require('commander')
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
