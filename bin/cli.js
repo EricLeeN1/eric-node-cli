6e782409fba61a86bedb7657527e5a4c42fa1cac
EricLeeN1 <179310009@qq.com>
2022-01-30 08:48:39 +0800

    feac:成功拿到命令行输入信息

--- bin/cli.js
+++ bin/cli.js
@@ -8,40 +8,58 @@
 // 用于检查入口文件是否正常执行
 console.log("eric-node-cli working~");
 
-const program = require('commander')
+const program = require("commander");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 const ejs = require("ejs");
 
-inquirer
-  .prompt([
-    {
-      type: "input", // input, number, confirm, list, checkbox ...
-      name: "name", // key 名
-      message: "You name", // 提示信息
-      default: "eric-node-cli", // 默认值
-    },
-  ])
-  .then((answers) => {
-    // 模板文件目录
-    const destUrl = path.join(__dirname, "templates");
-    // 生成文件目录
-    // process.cwd() 对应控制台所在目录
-    const cwdUrl = process.cwd();
-    console.log(cwdUrl);
-
-    fs.readdir(destUrl, (err, files) => {
-      if (err) throw err;
-      files.forEach((file) => {
-        // 使用 ejs 渲染对应的模版文件
-        // renderFile(模板文件地址，传入渲染数据)
-        ejs.renderFile(path.join(destUrl, file), answers).then((data) => {
-          // 生成ejs处理后的模板文件
-          fs.writeFileSync(path.join(cwdUrl, file), data);
-        });
-      });
-    });
-    // 打印用户输入结果
-    console.log(answers);
+// 定义命令和参数
+program
+  .command("create <app-name>")
+  .description("create a new application")
+  // -f or --force 为强制创建，如果创建的目录存在则直接覆盖
+  .option("-f, --force", "overwrite target directory if it exists")
+  .action((name, options) => {
+    console.log("name:", name, "options:", options);
   });
+
+program
+  // 配置版本号信息
+  .version(`v${require("../package.json").version}`)
+  .usage("<command> [option>]");
+
+// 解析用户执行命令传入参数
+program.parse(process.argv);
+
+// inquirer
+//   .prompt([
+//     {
+//       type: "input", // input, number, confirm, list, checkbox ...
+//       name: "name", // key 名
+//       message: "You name", // 提示信息
+//       default: "eric-node-cli", // 默认值
+//     },
+//   ])
+//   .then((answers) => {
+//     // 模板文件目录
+//     const destUrl = path.join(__dirname, "templates");
+//     // 生成文件目录
+//     // process.cwd() 对应控制台所在目录
+//     const cwdUrl = process.cwd();
+//     console.log(cwdUrl);
+
+//     fs.readdir(destUrl, (err, files) => {
+//       if (err) throw err;
+//       files.forEach((file) => {
+//         // 使用 ejs 渲染对应的模版文件
+//         // renderFile(模板文件地址，传入渲染数据)
+//         ejs.renderFile(path.join(destUrl, file), answers).then((data) => {
+//           // 生成ejs处理后的模板文件
+//           fs.writeFileSync(path.join(cwdUrl, file), data);
+//         });
+//       });
+//     });
+//     // 打印用户输入结果
+//     console.log(answers);
+//   });
